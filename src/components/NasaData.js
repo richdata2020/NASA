1a92781315beacaadd225566f47bbc042057b90c
RichVI <richardwang2050@gmail.com>
2020-05-31 11:11:07 -0700

    v1.0

--- src/components/NasaData.js
+++ src/components/NasaData.js
@@ -18,9 +18,9 @@ function formatDate(date) {
 export default function NasaData() {
     const[Nasa, setNasa] = useState([]);
     const[date, setDate] = useState([new Date()])
-    let nasaAPI = process.env.REACT_APP_NasaAPIKey;
+
     const decreaseDate = () => {
-        
+        let nasaAPI = process.env.REACT_APP_NasaAPIKey;
         let yesterday = new Date();
         let currentDate = new Date(date);
         yesterday.setDate(currentDate.getDate() - 1);
@@ -44,7 +44,7 @@ export default function NasaData() {
  
     useEffect(() => {
         axios
-        .get(`https://api.nasa.gov/planetary/apod?api_key=${nasaAPI}&date=` + formatDate(date))
+        .get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NasaAPIKey}&date=` + formatDate(date))
         .then(response => {
             console.log("API", response.data);
             setNasa(response.data);
